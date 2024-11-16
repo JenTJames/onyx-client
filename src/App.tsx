eb8fad9265b5fd1f0c38043161b5f11eec6be37f
Jen Thomas James <jenthomasjames@gmail.com>
2024-11-16 16:26:28 +0530

    dep: add tailwindcss and radix-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button, Theme } from "@radix-ui/themes";
 
 function App() {
-  return <></>;
+  return (
+    <Theme>
+      <Button variant="soft">Welcome to Onyx</Button>
+    </Theme>
+  );
 }
 
 export default App;
