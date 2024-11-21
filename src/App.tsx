4b0b55064504ea7954e6a5d37730207f99853e53
Jen Thomas James <jenthomasjames@gmail.com>
2024-11-21 20:42:58 +0530

    feat: use custom router component and sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import "./App.css";
 import { useContext } from "react";
-import { Button, Theme } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 import ThemeContext from "./store/ThemeContext";
+import { Toaster } from "sonner";
+import Router from "./components/Router";
 
 function App() {
   const themeCtx = useContext(ThemeContext);
 
   if (!themeCtx) throw new Error("App must be used inside a ThemeContext");
-  const { theme, toggleTheme } = themeCtx;
+  const { theme } = themeCtx;
 
   return (
-    <Theme accentColor="purple" appearance={theme || "light"}>
-      <Button onClick={toggleTheme} variant="soft">
-        Welcome to Onyx
-      </Button>
+    <Theme accentColor="purple" appearance={theme || "light"} radius="large">
+      <Toaster theme={theme} richColors closeButton />
+      <Router />
     </Theme>
   );
 }
