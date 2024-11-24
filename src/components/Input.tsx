5d18a1c94e806ff9efc7a27cfa390b6b19bdb771
Jen Thomas James <jenthomasjames@gmail.com>
2024-11-24 13:15:37 +0530

    feat: add eye buttons to password field automatically and reduce error message size

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { Controller } from "react-hook-form";
-import { Text, TextField } from "@radix-ui/themes";
+import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import InputProps from "../types/props/InputProps.interface";
+import { IconButton, Text, TextField } from "@radix-ui/themes";
+import InputTypes from "../types/InputTypes.type";
 
 const Input: React.FC<InputProps> = ({
   name,
   label,
   control,
   rules,
-  type,
+  type = "text",
   startAdornment,
   endAdornment,
 }) => {
+  const [inputType, setInputType] = useState<InputTypes>(type);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const passwordVisibilityHandler = () => {
+    setShowPassword((prevShowPassword) => {
+      const newShowPassword = !prevShowPassword;
+      setInputType(newShowPassword ? "text" : "password");
+      return newShowPassword;
+    });
+  };
+
   return (
     <Controller
       name={name}
@@ -21,7 +35,7 @@ const Input: React.FC<InputProps> = ({
         <div className="flex flex-col gap-1 w-full">
           <TextField.Root
             {...field}
-            type={type ? type : "text"}
+            type={inputType}
             size="3"
             placeholder={label}
             variant={fieldState.invalid ? "soft" : "surface"}
@@ -31,11 +45,22 @@ const Input: React.FC<InputProps> = ({
               <TextField.Slot side="left">{startAdornment}</TextField.Slot>
             )}
             {endAdornment && (
-              <TextField.Slot side="left">{endAdornment}</TextField.Slot>
+              <TextField.Slot side="right">{endAdornment}</TextField.Slot>
+            )}
+            {type === "password" && (
+              <TextField.Slot side="right">
+                <IconButton
+                  type="button"
+                  variant="ghost"
+                  onClick={passwordVisibilityHandler}
+                >
+                  {showPassword ? <EyeClosedIcon /> : <EyeOpenIcon />}
+                </IconButton>
+              </TextField.Slot>
             )}
           </TextField.Root>
           {fieldState.invalid && (
-            <Text color="crimson" weight="light" size="2">
+            <Text color="crimson" size="1">
               {fieldState.error?.message}
             </Text>
           )}
