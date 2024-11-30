f68d5984c123f8fc3c79706e74246cf98fb1bae5
Jen Thomas James <jenthomasjames@gmail.com>
2024-11-30 18:35:48 +0530

    fix: make requestData method in useHttp generic to handle return types properly

--- src/hooks/use-http.ts
+++ src/hooks/use-http.ts
@@ -1,35 +1,50 @@
 import { useCallback, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+interface HttpResponse<T> {
+  data: T | null;
+  isError: boolean;
+  message: string;
+  error: AxiosError | null;
+}
 
 const useHttp = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const requestData = useCallback(
-    async (
+    async <T>(
       endpoint: string,
-      method: string = "GET",
+      method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
       data: object | null = null
-    ) => {
+    ): Promise<HttpResponse<T>> => {
       setIsLoading(true);
-      let response;
       try {
-        response = await axios({
-          method: method,
-          data: data,
+        const response: AxiosResponse<T> = await axios({
+          method,
           url: import.meta.env.VITE_BACKEND_BASE_URL + endpoint,
+          data,
         });
-        return { data: response, isError: false, message: "", error: null };
-      } catch (error) {
-        const message =
-          error.response?.status === 500
-            ? "Internal Server Error"
-            : "Something went wrong. Please try again later";
-        const err = {
-          isError: true,
-          error,
-          message,
+
+        return {
+          data: response.data,
+          isError: false,
+          message: "Request successful",
+          error: null,
         };
-        return err;
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        const message =
+          axiosError.response?.status === 500
+            ? "Internal Server Error"
+            : axiosError.message ||
+              "Something went wrong. Please try again later";
+
+        return {
+          data: null,
+          isError: true,
+          message,
+          error: axiosError,
+        };
       } finally {
         setIsLoading(false);
       }
---
+++ src/types/User.interface.ts
@@ -0,0 +1,9 @@
+export default interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  email: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
