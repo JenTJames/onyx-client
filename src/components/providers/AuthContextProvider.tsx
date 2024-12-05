0f31b3a746cd29be374348014599f0a0726adb77
Jen Thomas James <jenthomasjames@gmail.com>
2024-12-05 20:33:47 +0530

    feat: refresh user details in auth context on page refresh

--- src/components/providers/AuthContextProvider.tsx
+++ src/components/providers/AuthContextProvider.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import User from "../../types/User.interface";
 import AuthContext from "../../store/AuthContext";
+import useHttp from "../../hooks/use-http";
+import { toast } from "sonner";
 
 const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
+  const { requestData } = useHttp();
+
   const clearUser = () => setLoggedInUser(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userId: number = parseInt(
+        localStorage.getItem("userId") || "0",
+        10
+      );
+      if (!userId) return;
+      const response = await requestData<User>(`/users/${userId}`);
+      if (response.isError) return toast.error(response.message);
+      setLoggedInUser(response.data);
+    };
+    if (localStorage.getItem("userId")) fetchUser();
+  }, [requestData]);
 
   return (
     <AuthContext.Provider
