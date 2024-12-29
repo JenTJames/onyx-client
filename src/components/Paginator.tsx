1f5b0ed25ee218eb272d59f9922e94120dcd8211
Jen Thomas James <jenthomasjames@gmail.com>
2024-12-29 15:18:33 +0530

    style: make paginator responsive

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -92,7 +92,7 @@ const Paginator = ({
       ) : (
         <>
           {/* Display items */}
-          <div className="grid grid-cols-4 gap-3">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
             {displayedItems.map((item) => (
               <SummaryCard
                 key={item.id}
